41442ba461a5c4abf32a0e62f88f008e5b4d5fde
Angela Kim <angelakim318@gmail.com>
2024-02-28 15:42:14 -0500

    updated registration form fields

--- backend/src/main/java/com/project/backend/config/JwtAuthorizationFilter.java
+++ backend/src/main/java/com/project/backend/config/JwtAuthorizationFilter.java
@@ -1,7 +1,6 @@
 package com.project.backend.config;
 
 import com.project.backend.util.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
